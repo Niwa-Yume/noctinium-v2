3b24c646a410fa71e8cd26b2eeb9312dd27b31b3
Julien Castro <julien.castro@protonmail.com>
2024-09-06 21:18:46 +0200

    menu header responsive

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,19 @@
-import { Component, HostListener } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  // Ajoutez ici toute logique spécifique au header si nécessaire
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
 }
