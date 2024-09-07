64a00d87c41b4f5b6b5a822398b9cf98e67064e8
Julien Castro <julien.castro@protonmail.com>
2024-09-07 12:12:29 +0200

    ajout icon connexion + lien autour de moi

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,19 +1,30 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  // Variable pour contrôler l'état du menu (ouvert/fermé)
   isMenuOpen = false;
 
+  // Méthode pour basculer l'état du menu
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
   }
 
+  // Méthode pour fermer le menu
   closeMenu() {
     this.isMenuOpen = false;
+  }
+
+  // Méthode pour gérer l'ouverture du modal de connexion/inscription
+  toggleLoginModal() {
+    console.log('Ouverture du modal de connexion/inscription');
+    // Ici, vous pouvez ajouter la logique pour ouvrir/fermer le modal
   }
 }
