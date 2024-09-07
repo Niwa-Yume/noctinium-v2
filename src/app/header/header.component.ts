2744d20c56aad87ee471f8463e284b886e3f42ab
Julien Castro <julien.castro@protonmail.com>
2024-09-07 13:06:15 +0200

    validation de la page d'accueil

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,22 +9,18 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  // Variable pour contrôler l'état du menu (ouvert/fermé)
   isMenuOpen = false;
 
-  // Méthode pour basculer l'état du menu
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
   }
 
-  // Méthode pour fermer le menu
   closeMenu() {
     this.isMenuOpen = false;
   }
 
-  // Méthode pour gérer l'ouverture du modal de connexion/inscription
   toggleLoginModal() {
     console.log('Ouverture du modal de connexion/inscription');
-    // Ici, vous pouvez ajouter la logique pour ouvrir/fermer le modal
+    // Logique pour ouvrir/fermer le modal
   }
 }
